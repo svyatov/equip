87e1e5cc0d94fa20dd5ede4a28a2044e0541f3f9
Leonid Svyatov <leonid@svyatov.com>
2026-09-25 19:27:54 +0300

    fix(cli): reject positional arguments
    equip ignored any argument after the flags. Flag handling moves into run so tests can cover --help, --version and the rejection. Refs #14.

--- main.go
+++ main.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"runtime/debug"
 
@@ -13,25 +15,33 @@ import (
 	"github.com/svyatov/equip/internal/equip"
 )
 
+const usage = "Usage: equip [--help] [--version]\n\nOpens the extensions of the Project in the working directory."
+
 func main() {
-	flag.CommandLine.SetOutput(os.Stdout)
-	flag.Usage = func() {
-		fmt.Println("Usage: equip [--help] [--version]\n\nOpens the extensions of the Project in the working directory.")
-	}
-	version := flag.Bool("version", false, "print the build version")
-	flag.Parse()
-	if *version {
-		info, _ := debug.ReadBuildInfo()
-		fmt.Println("equip", info.Main.Version)
-		return
-	}
-	if err := run(); err != nil {
+	if err := run(os.Args[1:], os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, "equip:", err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(args []string, stdout io.Writer) error {
+	fs := flag.NewFlagSet("equip", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+	version := fs.Bool("version", false, "print the build version")
+	if err := fs.Parse(args); errors.Is(err, flag.ErrHelp) {
+		_, err = fmt.Fprintln(stdout, usage)
+		return err
+	} else if err != nil {
+		return err
+	}
+	if fs.NArg() > 0 {
+		return fmt.Errorf("unexpected argument %q\n\n%s", fs.Arg(0), usage)
+	}
+	if *version {
+		info, _ := debug.ReadBuildInfo()
+		_, err := fmt.Fprintln(stdout, "equip", info.Main.Version)
+		return err
+	}
 	m, err := equip.MachineFromEnv()
 	if err != nil {
 		return err
